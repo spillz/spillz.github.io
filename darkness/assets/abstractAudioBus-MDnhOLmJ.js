1278de7161381f3beeb8460204465eb716d96294
Damien Moore <damienlmoore@gmail.com>
2026-08-18 01:56:22 -0400

    web of darkness 3

---
+++ darkness/assets/abstractAudioBus-MDnhOLmJ.js
@@ -0,0 +1,1 @@
+import{X as o}from"./index-DZ1tk7sx.js";class a extends o{constructor(s,t){super(s,t,3)}}export{a as A};
---
+++ darkness/assets/gpuRenderParticles.fragment-wudJqLP6.js
@@ -0,0 +1,25 @@
+import{g as e,h as n,aj as l,l as t,q as i,ak as s,i as c,n as S,j as g,k as f}from"./index-DZ1tk7sx.js";const r="gpuRenderParticlesPixelShader",o=`var diffuseSamplerSampler: sampler;var diffuseSampler: texture_2d<f32>;varying vUV: vec2f;varying vColor: vec4f;
+#include<clipPlaneFragmentDeclaration>
+#include<imageProcessingDeclaration>
+#include<logDepthDeclaration>
+#include<helperFunctions>
+#include<imageProcessingFunctions>
+#include<fogFragmentDeclaration>
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {
+#include<clipPlaneFragment>
+let textureColor: vec4f=textureSample(diffuseSampler,diffuseSamplerSampler,input.vUV);var baseColor: vec4f=textureColor*input.vColor;
+#ifdef BLENDMULTIPLYMODE
+let alpha: f32=input.vColor.a*textureColor.a;baseColor=vec4f(baseColor.rgb*alpha+vec3f(1.0)*(1.0-alpha),baseColor.a);
+#endif
+#include<logDepthFragment>
+#include<fogFragment>(color,baseColor)
+#ifdef IMAGEPROCESSINGPOSTPROCESS
+baseColor=vec4f(toLinearSpaceVec3(baseColor.rgb),baseColor.a);
+#else
+#ifdef IMAGEPROCESSING
+baseColor=vec4f(toLinearSpaceVec3(baseColor.rgb),baseColor.a);baseColor=applyImageProcessing(baseColor);
+#endif
+#endif
+fragmentOutputs.color=baseColor;}
+`;e.ShadersStoreWGSL[r]||(e.ShadersStoreWGSL[r]=o);const p=[n,l,t,i,s,c,S,g,f];for(const a of p)e.IncludesShadersStoreWGSL[a.name]||(e.IncludesShadersStoreWGSL[a.name]=a.shader);const d={name:r,shader:o};export{d as gpuRenderParticlesPixelShaderWGSL};
